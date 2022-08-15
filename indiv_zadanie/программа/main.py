92b76a8da1f5b37040ba4bab5c4c4b5f07242757
IZVARYCHEV <ivan.zvarychev@mail.ru>
2022-08-15 13:06:22 +0500

    доделка

--- indiv_zadanie/программа/main.py
+++ indiv_zadanie/программа/main.py
@@ -6,6 +6,20 @@ import os.path
 from bs4 import BeautifulSoup
 from glob import glob
 
+# исключение папок из проверок
+exception = 'sh_us'
+
+#перехорд в каталог и удаление файла output.txt, если он есть
+os.chdir('//106-dc01//aida//Sbor//test')  # выбрать каталог
+print(f'current DIR: {os.getcwd()}')  # отображение текущего
+
+if os.path.isfile("//106-dc01//aida//Sbor//test//output.txt"):
+    os.remove("//106-dc01//aida//Sbor//test//output.txt")
+    print("File output.txt deleted success")
+else:
+    print("File output.txt doesn't exists!")
+
+
 # создаю пустой список, в него "накоплю" (+= [y]) все каталоги с указанного пути в os.walk,
 # кстати, walk создает каталоги рекурсивно, что очень хорошо для решения данной задачи
 #
@@ -19,20 +33,30 @@ Directory = []
 for address, dirs, files in os.walk('//106-dc01//aida//Sbor//test'):
     y = re.sub(r'\\', '//', address)
     Directory += [y]
-Directory.pop(0)  # удаление первого корневого каталога (//test) указанного в os.walk.
+Directory.pop(0)# удаление первого корневого каталога (//test) указанного в os.walk.
 # Он нам не нужен, т.к. в нем нет htm файлов согласно исходным данным.
+
+#исключение папок из работы парсера (исключение происходит из строковой переменной exception).
+#создается пустой список происходит проверка по строкам(в условии), далее перенос обратно в список
+S = ''
+for dir in Directory:
+    if (not exception in dir):
+        S += (dir + '\n')
+rows = S.splitlines()
+Directory = rows
 print(Directory)
-print()
 
 # далее обращаюсь к подкаталогам списка Directory
 # в цикле перечисляемая переменная dir предстает в виде списка,
 # ее я использую для построения списка файлов в данном каталоге (listdir),
 # а так же для указания текущего каталога интерпретатору(os.chdir),
 # чтобы тот понимал с какого католога ему считывать файлы
+
 for dir in Directory:
     os.chdir(dir)
-    print(f'current DIR {os.getcwd()}')  # текущий
+    print(f'current DIR: {os.getcwd()}')  # отображение текущего
     new_list = listdir(dir)
+    # print(new_list)
     W = []
     # т.к. исходные данные содержат папки внутри подпапок, переданных в new_list,
     # то listdir считывает и их тоже. Для того, чтобы убрать их из списка
@@ -41,29 +65,41 @@ for dir in Directory:
     # и из списка new_list извлекаю индекс, содержащий значение 'sh_us' и все кроме 'sh_us'
     # добавляю в ранее созданный еще один список W
     for i in range(len(new_list)):
-        if new_list[i] != 'sh_us':
+        if new_list[i] != exception:
             W.append(new_list[i])
-    # цикл перечисляет файлы в подкаталогах из списка W, открывает их, считывает в переменную
+    # цикл перебирает файлы в подкаталогах из списка W, открывает их, считывает в переменную
     # result и далее уже "парсер" через библиотеку BeatifulSoup, извлекает разметку,
     # разделяет ее на строки и согласно ключевому индивидуальному заданию
-    # (вывести список файлов в которых упоминается заданное выражение)  производит поиск
-    # перечисляемых в цикле файлах
-    #
-    # далее результат передается в строковую переменную v, построчно, с дозаписываением файла
-    # (режим записи 'a') методом out производится заполнение файла по указанному пути.
+    # (вывести список файлов в которых упоминается заданное выражение или строка) происходит поиск
+    # перечисляемых в цикле файлов.
+    # далее, информация с парсера через функцию get_text преобразуется в строки, затем в цикле
+    # происходит перебор элементов списка words и вывод строк с искомыми словами в файл output.txt
 
     for htmlfiles in W:
         f = open(htmlfiles, 'r')
         result = f.read()
         soup = BeautifulSoup(result, 'lxml')
-        strings = soup.find_all(string=re.compile('Windows 7'))
+        res_str = soup.get_text()
+
+        words = [
+            'Имя компьютера  ',
+            'Имя пользователя  ',
+            'Тип ЦП  ',
+            'Системная плата  ',
+            'Системная память  ',
+        ]
+        otbor = ''
         v = ''
-        for txt in strings:
-            # это строки вывод строк, в каких именно встречается указанное в re.compile
-            # print(" ".join(txt.split()))
-            # это вывод названия файлов в которых упоминается заданное выражения
-            v = htmlfiles
-        # запись в файл
+        for w in words:
+            for stroka in res_str.split('\n'):
+                v = htmlfiles
+                if w in stroka:
+                    otbor += (stroka + '\n')
+        rows = otbor.splitlines()
+        unique_rows = dict.fromkeys(rows)
+        new_text = "\n".join(unique_rows)
+        # print(v)
+        # print()
+        # print(new_text + '\n')
         with open('//106-dc01//aida//Sbor//test//output.txt', 'a') as out:
-            print(v, file=out)
-        print(v)
+            print(new_text, v, file=out)
